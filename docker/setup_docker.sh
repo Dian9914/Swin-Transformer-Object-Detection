b5b1ed2fe4ac6accf6d928000ba0833e726a80a2
dian9914 <edusotelo1999@gmail.com>
2022-03-10 13:09:48 +0100

    dockerfile

---
+++ docker/setup_docker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#docker instalation 
+apt-get update
+
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+
+#nvidia docker toolkit instalation
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+apt-get update
+apt-get install -y nvidia-docker2
+systemctl restart docker
+
+#docker image build
+docker build -t swin ./
+
+#docker container run
+mkdir ~/docker_output
+docker run --gpus all --shm-size=16g -it -v ~/docker_output:/workspace/output swin
+
