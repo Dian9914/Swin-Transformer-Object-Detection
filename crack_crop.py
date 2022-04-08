7a8e294bd7836f8c698fe65980ecd547c7b3d57f
dian9914 <edusotelo1999@gmail.com>
2022-04-08 09:53:20 +0200

    dir_inference updated to output results as json files

---
+++ crack_crop.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+import cv2
+import json
+import random
+import os
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Checks and fixes an entire dataset')
+    parser.add_argument('dir', help='dataset directory')
+    parser.add_argument('--auto', default= True, help ='Lets you go one image at a time')
+    parser.add_argument('--out', default='./output/', help='Directory to save the output')
+    args = parser.parse_args()
+    return args
+
+args = parse_args()
+
+for split in ['train','val']:
+        with open(os.path.join(args.dir,f'annotations/codebrim_{split}.json'),'r') as f:
+                ann = json.load(f)
+
+                image_id = 0
+                while image_id < len(ann['images']):
+                        entry=ann['images'][image_id]
+                        img=entry['file_name']
+                        id=entry['id']
+                        path = os.path.join(args.dir,f'{split}/{img}')
+                        print(entry)
+                        if not args.auto: cv2.namedWindow(f'{split} bnbox', cv2.WINDOW_NORMAL) 
+                        keycode=0
+                        while(keycode!=27 and keycode!=13 and keycode!=97):
+
+                        
+                                image = cv2.imread(path)
+                                spallated_bars = list()
+                                corroded_efflores = list()
+                                try:
+                                        for index in range(len(ann['annotations'])):
+                                                
+                                                note = ann['annotations'][index]
+                                                if note['image_id']==id:
+                                                        if keycode==115:
+                                                                del ann['annotations'][index]
+                                                                continue
+                                                        bbox=note['bbox']
+                                                        note_id = note['id']
+                                                        category=int(note['category_id'])
+                                                        if category==1: 
+                                                                defect='Crack'
+                                                                color=(255,0,0)
+
+                                                        elif category==3: 
+                                                                defect='Spallation'
+                                                                spallated_bars.append((bbox,index))
+                                                                color=(0,255,0)
+                                                        elif category==4: 
+                                                                defect='Efflorescence'
+                                                                corroded_efflores.append((bbox,index))
+                                                                color=(0,0,255)
+                                                        elif category==5: 
+                                                                defect='ExposedBars'
+                                                                spallated_bars.append((bbox,index))
+                                                                color=(255,255,0)
+                                                        elif category==6: 
+                                                                defect='CorrosionStain'
+                                                                spallated_bars.append((bbox,index))
+                                                                corroded_efflores.append((bbox,index))
+                                                                color=(255,0,255)
+                                                        elif category==7: 
+                                                                defect='SpallatedBars'
+                                                                color=(0,255,255)
+                                                        elif category==8: 
+                                                                defect='CorrodedEfflorescence'
+                                                                color=(255,255,255)
+                                                        else: defect='ups'
+
+                                                        start_point = (round(bbox[0]), round(bbox[1]))
+                                                        end_point = (round(bbox[0]+bbox[2]), round(bbox[1]+bbox[3]))
+
+                                                        if category==1:
+                                                                cropped_image = cv2.imread(path)
+                                                                cropped_image = cropped_image[start_point[1]:end_point[1],start_point[0]:end_point[0]]
+                                                                if cropped_image.shape[0]>=400 and cropped_image.shape[1]>=400:
+                                                                        cropped_image = cropped_image[0:min(cropped_image.shape[:2]),0:min(cropped_image.shape[:2])]
+                                                                        cropped_image=cv2.resize(cropped_image, (800,800), interpolation = cv2.INTER_AREA)
+                                                                        out_path=os.path.join(args.out,f'crack_{note_id}.jpg')
+                                                                        cv2.imwrite(out_path,cropped_image)
+                                                                        print(f'Image saved in {out_path}')
+
+                                                        text_point = (start_point[0],round(bbox[1]+bbox[3]*random.random()))
+                                                        image = cv2.rectangle(image, start_point, end_point, color, 5)
+                                                        image = cv2.putText(image,defect,text_point,cv2.FONT_HERSHEY_SIMPLEX,3,color,5)
+                                
+                                except IndexError:
+                                        pass                       
+
+                                if keycode==115:
+                                        break
+                                if args.auto: keycode = 13
+                                else:
+                                        cv2.imshow(f'{split} bnbox',image)
+                                        keycode=cv2.waitKey()
+                                
+                                if keycode==13: #intro
+                                        image_id=image_id+1
+                                
+                                
+                        if keycode==27:
+                                break    
--- tools/dir_inference.py
+++ tools/dir_inference.py
@@ -3,12 +3,19 @@ import argparse
 import cv2
 import torch
 import os
+import os.path as osp
+import json
+import numpy as np
 
 import time
 
 from mmdet.apis import inference_detector, init_detector
+from mmdet.datasets import CocoDataset
+
+
 
 def parse_args():
+    classes = ['Crack','Spallation','Efflorescence','ExposedBars','CorrosionStain']
     parser = argparse.ArgumentParser(description='Does an inference to an entire directory')
     parser.add_argument('dir', help='directory to inference')
     parser.add_argument('config', help='test config file path')
@@ -19,11 +26,14 @@ def parse_args():
         '--device', type=str, default='cuda:0', help='CPU/CUDA device option')
     parser.add_argument(
         '--score-thr', type=float, default=0.5, help='bbox score threshold')
+    parser.add_argument(
+        '--classes', type=list, default=classes, help='classes to detect')
     args = parser.parse_args()
     return args
 
 def main():
     args = parse_args()
+    classes = args.classes
 
     o_path_def=os.path.join(args.out_dir,'defect/')
     o_path_nodef=os.path.join(args.out_dir,'no_defect/')
@@ -50,22 +60,32 @@ def main():
 
         start=time.time()
         result = inference_detector(model, img)
+        result_dict = dict()
         elapsed_time=time.time()-start
 
         o_path=o_path_nodef
-
-
+        index = 0
         for defect_class in result:
+            result_dict[f'{classes[index]}']=dict()
+            defect_index = 0
             for defect in defect_class:
                 if defect[4]>=args.score_thr:
+                    result_dict[f'{classes[index]}'][f'defect_{defect_index}']=dict()
+                    result_dict[f'{classes[index]}'][f'defect_{defect_index}']['bbox']=np.ndarray.tolist(defect[:4])
+                    result_dict[f'{classes[index]}'][f'defect_{defect_index}']['score']=str(defect[4])
+                    defect_index = defect_index + 1
                     o_path=o_path_def
                     print('defect')
 
+            index=index+1
+
+        with open(osp.join(args.out_dir,f'result_{img_name[:-4]}.json'), 'w') as outf:
+            json.dump(dict(result_dict), outf)
         
         o_path=os.path.join(o_path, img_name)
 
         cv2.imwrite(o_path,model.show_result(
-            img, result, score_thr=args.score_thr, wait_time=0, show=False))       
+            img, result, score_thr=args.score_thr, wait_time=0, show=False, thickness=15, font_size=40))       
 
         print(f'Inference time: {elapsed_time}')
 
